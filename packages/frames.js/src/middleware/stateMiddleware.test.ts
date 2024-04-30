c5c40a0ba2dd5ffa3278e995465e11e49c0369d6
Michal Kvasničák <michal.kvasnicak@gmail.com>
2024-04-30 09:55:02 +0200

    feat: state signing

--- packages/frames.js/src/middleware/stateMiddleware.test.ts
+++ packages/frames.js/src/middleware/stateMiddleware.test.ts
@@ -1,13 +1,31 @@
 /* eslint-disable no-console -- we expect the usage of console.log */
+import { createHmac } from "node:crypto";
 import type { FramesContext } from "../core/types";
 import { stateMiddleware } from "./stateMiddleware";
 
+function createSignature(state: any, secret: string): string {
+  return createHmac("sha256", secret)
+    .update(JSON.stringify(state))
+    .digest("hex");
+}
+
 describe("stateMiddleware", () => {
+  let consoleWarnSpy: jest.SpyInstance;
+
+  beforeAll(() => {
+    consoleWarnSpy = jest.spyOn(console, "warn");
+  });
+
+  beforeEach(() => {
+    consoleWarnSpy.mockReset();
+  });
+
   it("decodes JSON state from frame message and assigns it to ctx", async () => {
     const state = { foo: "bar" };
     const ctx = {
       message: { state: JSON.stringify(state) },
       initialState: {},
+      request: new Request("http://localhost", { method: "POST" }),
     };
     const mw = stateMiddleware();
     const next = jest.fn();
@@ -18,20 +36,22 @@ describe("stateMiddleware", () => {
   });
 
   it("uses initial state and warns user if JSON decode failed", async () => {
-    console.warn = jest.fn();
     const state = { foo: "bar" };
     const ctx = {
       message: { state },
       initialState: { initial: true },
+      request: new Request("http://localhost", { method: "POST" }),
     };
     const mw = stateMiddleware();
     const next = jest.fn();
 
-    expect(console.warn).not.toHaveBeenCalled();
+    expect(consoleWarnSpy).not.toHaveBeenCalled();
 
     await mw(ctx as unknown as FramesContext, next);
 
-    expect(console.warn).toHaveBeenCalled();
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      expect.stringContaining("Failed to parse state from frame message")
+    );
 
     expect(next).toHaveBeenCalledWith({ state: { initial: true } });
   });
@@ -39,6 +59,7 @@ describe("stateMiddleware", () => {
   it("uses initial state if there is no message", async () => {
     const ctx = {
       initialState: { initial: true },
+      request: new Request("http://localhost", { method: "POST" }),
     };
     const mw = stateMiddleware();
     const next = jest.fn();
@@ -53,12 +74,135 @@ describe("stateMiddleware", () => {
     const ctx = {
       message: { state: JSON.stringify(state) },
       initialState: {},
+      request: new Request("http://localhost", { method: "POST" }),
     };
     const mw = stateMiddleware();
     const next = jest.fn().mockReturnValue({ image: "/test" });
 
     const result = await mw(ctx as unknown as FramesContext, next);
 
-    expect(result).toEqual({ image: "/test", state });
+    expect(result).toEqual({ image: "/test", state: JSON.stringify(state) });
+  });
+
+  it("warns if state is returned but we are on initial frame", async () => {
+    const ctx = {
+      initialState: {},
+      request: new Request("http://localhost", { method: "GET" }),
+    };
+    const mw = stateMiddleware();
+    const next = jest
+      .fn()
+      .mockReturnValue({ image: "/test", state: { foo: "bar" } });
+
+    expect(consoleWarnSpy).not.toHaveBeenCalled();
+
+    await mw(ctx as unknown as FramesContext, next);
+
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      expect.stringContaining("State is not supported on initial request")
+    );
+  });
+
+  describe("stateSigningSecret", () => {
+    it("signs state with provided secret", async () => {
+      const state = { foo: "bar" };
+      const ctx = {
+        initialState: {},
+        stateSigningSecret: "test",
+        request: new Request("http://localhost", { method: "POST" }),
+      };
+
+      const mw = stateMiddleware();
+      const next = jest.fn().mockResolvedValue({ image: "/test", state });
+
+      const result = await mw(ctx as unknown as FramesContext, next);
+
+      expect(result).toMatchObject({
+        image: "/test",
+        state: JSON.stringify({
+          data: state,
+          __sig: createSignature(state, "test"),
+        }),
+      });
+    });
+
+    it("warns if state signature verification failed and uses initial state", async () => {
+      const state = { foo: "bar" };
+      const ctx = {
+        message: {
+          state: JSON.stringify({
+            data: state,
+            __sig: createSignature(state, "test2"),
+          }),
+        },
+        initialState: { initial: true },
+        stateSigningSecret: "test1",
+        request: new Request("http://localhost", { method: "POST" }),
+      };
+
+      const mw = stateMiddleware();
+      const next = jest.fn().mockResolvedValue({ image: "/test", state });
+
+      await mw(ctx as unknown as FramesContext, next);
+
+      expect(consoleWarnSpy).toHaveBeenCalledWith(
+        expect.stringContaining("State signature verification failed")
+      );
+
+      expect(next).toHaveBeenCalledWith({
+        state: { initial: true },
+      });
+    });
+
+    it("warns that state is signed and uses the state if secret is not provided", async () => {
+      const state = { foo: "bar" };
+      const ctx = {
+        message: {
+          state: JSON.stringify({
+            data: state,
+            __sig: createSignature(state, "test"),
+          }),
+        },
+        initialState: { initial: true },
+        request: new Request("http://localhost", { method: "POST" }),
+      };
+
+      const mw = stateMiddleware();
+      const next = jest.fn().mockResolvedValue({ image: "/test", state });
+
+      await mw(ctx as unknown as FramesContext, next);
+
+      expect(consoleWarnSpy).toHaveBeenCalledWith(
+        expect.stringContaining("State is signed but no secret is provided")
+      );
+
+      expect(next).toHaveBeenCalledWith({
+        state,
+      });
+    });
+
+    it("uses signed state if the secret matches", async () => {
+      const state = { foo: "bar" };
+      const ctx = {
+        message: {
+          state: JSON.stringify({
+            data: state,
+            __sig: createSignature(state, "test"),
+          }),
+        },
+        initialState: { initial: true },
+        stateSigningSecret: "test",
+        request: new Request("http://localhost", { method: "POST" }),
+      };
+
+      const mw = stateMiddleware();
+      const next = jest.fn().mockResolvedValue({ image: "/test", state });
+
+      await mw(ctx as unknown as FramesContext, next);
+
+      expect(next).toHaveBeenCalledWith({
+        state,
+      });
+    });
   });
 });
